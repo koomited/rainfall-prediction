8a7826b29cbe2b83089b988585cf91b34930bb37
koomited <koomi@aims.ac.za>
2025-09-23 09:49:30 +0000

    Data ingestion completed

---
+++ src/mlProject/components/data_ingestion.py
@@ -0,0 +1,20 @@
+import pandas as pd
+from mlProject.utils.common import get_size
+from mlProject import logger
+from pathlib import Path
+from mlProject.entity.config_entity import (DataIngestionConfig)
+import os
+
+class DataIngestion:
+    def __init__(self, config: DataIngestionConfig):
+        self.config = config
+
+    def download_data(self):
+        if not os.path.exists(self.config.local_data_file):
+            data = pd.read_csv(self.config.source_URL)
+            data.to_csv(self.config.local_data_file, index=False)
+            logger.info(f"data download with following info: shape:{data.shape}")
+        else:
+            logger.info(f"File already exists with size: {get_size(Path(self.config.local_data_file))} bytes")
+            
+        
