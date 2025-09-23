592c208edcb9070e695b4880ceae073d02a33328
koomited <koomi@aims.ac.za>
2025-09-23 13:23:50 +0000

    data validation added

--- main.py
+++ main.py
@@ -1,6 +1,6 @@
 from mlProject import logger
 from mlProject.pipeline.stage_01_data_ingestion import DataIngestionPipeline
-# from mlProject.pipeline.stage_02_data_validation import  DataValidationPipeline
+from mlProject.pipeline.stage_02_data_validation import  DataValidationPipeline
 # from mlProject.pipeline.stage_03_data_transformation import DataTransformationPipeline
 # from mlProject.pipeline.stage_04_model_trainer import ModelTrainerPipeline
 # from mlProject.pipeline.stage_05_model_evaluation import ModelEvaluationPipeline
@@ -9,6 +9,17 @@ STAGE_NAME = "Data Ingestion stage"
 try:
    logger.info(f">>>>>> stage {STAGE_NAME} started <<<<<<") 
    data_ingestion = DataIngestionPipeline()
+   data_ingestion.main()
+   logger.info(f">>>>>> stage {STAGE_NAME} completed <<<<<<\n\nx==========x")
+except Exception as e:
+        logger.exception(e)
+        raise e
+
+
+STAGE_NAME = "Data Validation stage"
+try:
+   logger.info(f">>>>>> stage {STAGE_NAME} started <<<<<<") 
+   data_ingestion = DataValidationPipeline()
    data_ingestion.main()
    logger.info(f">>>>>> stage {STAGE_NAME} completed <<<<<<\n\nx==========x")
 except Exception as e:
