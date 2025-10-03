16b8aa765eb4e0fef7321091e162320ece42030f
koomited <koomi@aims.ac.za>
2025-10-03 18:12:32 +0000

    integration tests

---
+++ integration-test/test_docker.py
@@ -0,0 +1,37 @@
+# pylint: disable=duplicate-code
+import json
+
+import requests
+from deepdiff import DeepDiff
+
+with open('event.json', 'rt', encoding='utf-8') as f_in:
+    event = json.load(f_in)
+
+
+url = "http://localhost:8080/2015-03-31/functions/function/invocations"
+actual_response = requests.post(url, json=event, timeout=20).json()
+print("Actual response:")
+
+print(json.dumps(actual_response, indent=2))
+
+expected_response = expected_prediction = {
+        "predictions": [
+            {
+                'model': 'rainfall-prediction',
+                "version": "8de0cb304e844db8ae045f16c26c71db",
+                "prediction": {
+                    'rainfall': 0,
+                    'rainfall_history_id': actual_response["predictions"][0]["prediction"]["rainfall_history_id"]
+                    
+                },
+            }
+        ]
+    }
+
+diff = DeepDiff(actual_response, expected_response, significant_digits=3)
+print('diff=', diff)
+
+assert 'type_changes' not in diff
+assert 'values_changed' not in diff
+
+assert  actual_response == expected_response
---
+++ integration-test/test_kinesis.py
@@ -0,0 +1,47 @@
+# pylint: disable=duplicate-code
+import os
+import json
+from pprint import pprint
+
+import boto3
+from deepdiff import DeepDiff
+
+kinesis_endpoint = os.getenv("KINESIS_ENDPOINT_URL", "http://localhost:4566")
+
+kinesis_client = boto3.client(
+    "kinesis", region_name="us-east-1", endpoint_url=kinesis_endpoint
+)
+stream_name = os.getenv("PREDICTIONS_STREAM_NAME", 'ride_predictions')
+shard_id = "shardId-000000000000"
+
+shard_iterator_response = kinesis_client.get_shard_iterator(
+    StreamName=stream_name,
+    ShardId=shard_id,
+    ShardIteratorType='TRIM_HORIZON',
+)
+
+shard_iterator_id = shard_iterator_response["ShardIterator"]
+
+record_response = kinesis_client.get_records(ShardIterator=shard_iterator_id, Limit=1)
+
+
+records = record_response["Records"]
+
+pprint(records)
+
+assert len(records) == 1
+
+actual_record = json.loads(records[0]["Data"])
+
+pprint(actual_record)
+
+expected_record = {
+    'model': 'ride-duration-prediction-model',
+    'version': "Test123",
+    'prediction': {
+        'ride_duration': 18.120189001540375,
+        'ride_id': 256,
+    },
+}
+diff = DeepDiff(actual_record, expected_record, significant_digits=3)
+print('diff=', diff)
